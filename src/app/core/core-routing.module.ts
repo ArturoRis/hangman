7d8ad7c68e84d5c42568f5f50d88c4f254dd6e92
Fabio Siliberto <fabio.siliberto@gmail.com>
2020-04-18 18:37:42 +0200

    Add welcome component

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'welcome', pathMatch: 'full'
+  },
+  {
+    path: 'welcome', component: WelcomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CoreRoutingModule { }
+export class CoreRoutingModule {
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { PageComponent } from './components/page/page.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
     PageComponent,
-    MenuComponent
+    MenuComponent,
+    WelcomeComponent
   ],
   exports: [
     PageComponent
