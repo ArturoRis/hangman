fefd08ae9c160086991afd513aadbafae00a964b
Fabio Siliberto <fabio.siliberto@gmail.com>
2020-04-30 21:23:57 +0200

    Added tooltip and copy link

--- hangman-client/src/app/play/components/room/room.component.ts
+++ hangman-client/src/app/play/components/room/room.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../../../core/services/socket.service';
-import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { BaseComponent } from '../../../core/base-objects/base-component';
 import { PlayerInfoService } from '../../../core/services/player-info.service';
@@ -17,12 +16,12 @@ export class RoomComponent extends BaseComponent implements OnInit {
   players: Observable<PlayerInfo[]>;
   roomId: Observable<string>;
   amIMaster: Observable<boolean>;
+  tooltiptext = 'Clicca per copiare il link';
 
   constructor(
     private socketService: SocketService,
     private playerInfoService: PlayerInfoService,
     private gameStateService: GameStateService,
-    private route: ActivatedRoute,
   ) {
     super();
   }
@@ -40,5 +39,13 @@ export class RoomComponent extends BaseComponent implements OnInit {
 
   startGame() {
     this.gameStateService.initGame();
+  }
+
+  async getLink() {
+    const toCopy = location.href;
+    await navigator.clipboard.writeText(toCopy);
+    const oldTooltip = this.tooltiptext;
+    this.tooltiptext = 'Link copiato!';
+    setTimeout(() => this.tooltiptext = oldTooltip, 2000);
   }
 }
